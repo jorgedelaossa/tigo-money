134dc7ef5680d3f041e235ce45e6e4efc1fd94c0
johel <johel.espana@jellyfish.com>
2022-07-13 22:07:24 -0500

    some responsive issues was fixed

--- src/components/base/carousel/carousel.js
+++ src/components/base/carousel/carousel.js
@@ -11,8 +11,11 @@ const CarouselHome = (props) => {
         {props.data.items.map((item, i) => {
           return (
             <a href={item.description} target="_blank" rel="noreferrer">
-                <div key={i} className="main-carousel  " style={{ backgroundImage: `url(${item.url})` }} >
-                    
+                {/* <div key={i} className="main-carousel  " style={{ backgroundImage: `url(${item.url})` }} >
+                      
+                </div> */}
+                <div key={i} className="main-carousel d-flex align-items-center "  >
+                      <img className="img-carousel" alt={item.title} src={item.url} />
                 </div>
             </a>
             
--- src/components/base/cta/cta.js
+++ src/components/base/cta/cta.js
@@ -2,24 +2,25 @@ import "./cta.css";
 
 const Cta = (props) => {
   return (
-    <div className="container cta-container">
-      <div className="row ">
-        <div className="col-8  cta-col">
-          <div className="row ">
-            <div className="col-2 icon-container d-flex align-items-center">
-              <img src={props.data.icon?.url} alt="CTA" />
-            </div>
-            <div className="col-10 text-container d-flex align-items-center">
-              {props.data.text} 
-            </div>
+    <div className="container  cta-container">
+      <div className="  cta-col">
+        <div className="row ">
+          <div className="col-2  icon-container no-padding d-flex align-items-center">
+            <img className="img-cta" src={props.data.icon?.url} alt="CTA" />
           </div>
-        </div>
-        <div className="col-4 cta-col d-flex justify-content-end align-items-center p-2">
+          <div className="col-6 p-4  text-container d-flex align-items-center">
+            {props.data.text}
+          </div>
+          <div className="col-4   cta-col d-flex justify-content-end align-items-center p-2">
             <a href={props.data.ctaButton}>
               <button className="cta-button">{props.data.labelButton} </button>
             </a>
+          </div>
         </div>
       </div>
+      {/* <div className="col-4 debug cta-col d-flex justify-content-end align-items-center p-2">
+           
+        </div> */}
     </div>
   );
 };
