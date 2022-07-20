9d460789795f0a2775b6ca20c4d82d016fc768f8
johel <johel.espana@jellyfish.com>
2022-07-20 15:04:57 -0500

    account btn on mobile was fixed

--- src/components/base/topMenu/topMenu.js
+++ src/components/base/topMenu/topMenu.js
@@ -4,7 +4,7 @@ const TopMenu =(props)=> {
 
     return(
         <div className="top-menu d-flex justify-content-end">
-        <div className="col-4 col-md-2 account-btn  d-flex justify-content-center align-items-center">
+        <div className="col-6 col-md-2 account-btn  d-flex justify-content-center align-items-center">
             <a className="" href={props.urlAccount} target="_blank" rel="noreferrer">
               Tu cuenta Tigo Money
             </a>
